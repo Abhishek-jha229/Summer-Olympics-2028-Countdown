ca35e630b5c99f724e6f5b31a1788521a172839f
Abhishek-jha229 <jha83583@gmail.com>
2026-06-08 08:02:26 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const targetDate = new Date("July 14, 2028 00:00:00").getTime();
+
+function updateCountdown(){
+    const now = new Date().getTime();
+
+    const diff = targetDate - now;
+
+    const days = Math.floor(diff/(1000*60*60*24));
+
+    const hours=Math.floor((diff/(1000*60*60))%24);
+    const minutes= Math.floor((diff/(1000*60))%60);
+    const seconds= Math.floor((diff/(1000))%60);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+}
+
+
+setInterval(updateCountdown,1000);
+updateCountdown();
