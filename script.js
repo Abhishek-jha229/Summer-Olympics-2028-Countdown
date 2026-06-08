bca9d40b7952ff42b95b034f2d1691d64c9993ec
Abhishek-jha229 <jha83583@gmail.com>
2026-06-08 09:11:12 +0530

    Updated script.js
    Added countdown finish handling.

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function updateCountdown(){
 
     const diff = targetDate - now;
 
-    const days = Math.floor(diff/(1000*60*60*24));
+    if (diff < 0) {
+        document.body.innerHTML = "<h1>Olympics Started! 🎉</h1>";
+        return;
+    }
 
+
+    const days = Math.floor(diff/(1000*60*60*24));
     const hours=Math.floor((diff/(1000*60*60))%24);
     const minutes= Math.floor((diff/(1000*60))%60);
     const seconds= Math.floor((diff/(1000))%60);
